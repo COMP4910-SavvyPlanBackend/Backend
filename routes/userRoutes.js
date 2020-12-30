6d8ae086970df50445eadc9cd5ce78d62cc4a7e5
Mouhanedbr <rhouma30@outlook.com>
2020-12-30 12:50:55 -0800

    added the invite route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -12,7 +12,7 @@ router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
 
 router.get('/getProfile/:id', userController.getProfileById);
-
+router.post('/invite', authController.invite);
 //all below are protected
 
 router.patch('/updateMe', authController.protect, userController.updateMe);
